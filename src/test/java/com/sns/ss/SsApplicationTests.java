298e6b7ea0aeb065ef22a009a8962e4ee7cdb26e
Gonue <6017891@naver.com>
2023-03-27 06:52:46 +0900

    chore : 프로젝트 초기 설정

---
+++ src/test/java/com/sns/ss/SsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
