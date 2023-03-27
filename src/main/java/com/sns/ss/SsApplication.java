008759313f2daec909ad5ebd6f5f5a1e0f2a3b1c
Gonue <6017891@naver.com>
2023-03-28 05:13:58 +0900

    feat : Spring Data JPA 속성과 설정
    - jpa auditing 기능을 쓰기 위해 `JpaConfig` 추가
    - db 접근과 개발 편의를 위한 각종 부트 프로퍼티 적용

--- src/main/java/com/sns/ss/SsApplication.java
+++ src/main/java/com/sns/ss/SsApplication.java
@@ -3,8 +3,9 @@ package com.sns.ss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SsApplication {
 
     public static void main(String[] args) {
